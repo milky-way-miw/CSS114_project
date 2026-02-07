0380259d20b2ea5e0e2108d1773a973085fbc420
milky-way-miw <tananya.limj@gmail.com>
2026-02-07 06:45:06 +0000

    last of last

--- inverse.c
+++ inverse.c
@@ -5,7 +5,7 @@ int main() {
     double A[3][3] ,b[3];
     double det, inv[3][3], x[3];
     
-    //รับค่าเมทริก A
+    // รับค่าเมทริก A
     printf("Enter matrix A (3 x 3):\n");
     for (int i = 0; i < 3; i++){
         for (int j = 0; j < 3; j++){
@@ -13,11 +13,11 @@ int main() {
         }
     }
     
-    //เช็คว่ามีเมทริก b มั้ย
+    // เช็คว่ามีเมทริก b มั้ย
     printf("Enter 1 if have b matrix or 0 if dont have b matrix :");
     scanf("%d",&b_matrix);
 
-    //ถ้ามีให้รับค่าเมทริก b
+    // ถ้ามีให้รับค่าเมทริก b
     if(b_matrix == 1){
         printf("Enter b matrix (3 values):\n");
         for (int i = 0; i < 3; i++){
@@ -75,7 +75,7 @@ int main() {
         }
     }
 
-    //แสดงผล
+    // แสดงผล
     printf("Inverse Matrix =\n");
     for (int i = 0; i < 3; i++)
     {
